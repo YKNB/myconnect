1df1d05a000fc1adddabf4f847c62c3229f7c61d
karlitoyegbe <karlitoyegbe@gmail.com>
2025-11-13 10:52:45 +0000

    add config Firebase

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  projectId: "myconnect-fe85c",
+  appId: "1:693988692776:web:ee46e2a01e0eacb6a5761d",
+  storageBucket: "myconnect-fe85c.appspot.com",
+  apiKey: "AIzaSyD-IufgRk5Fz9DYmvVyZCTYCGAddGH5RmA",
+  authDomain: "myconnect-fe85c.firebaseapp.com",
+  messagingSenderId: "693988692776",
+  measurementId: "G-54CMDEBT0C"
+};
